169f0be6d29be8c4f32e3047c91a48e6972f850d
MiguelangelLozAl <miguelangellozanoa@gmail.com>
2022-11-07 12:24:28 -0800

    Simulator js

---
+++ call-sim-js/gaussianRandom.mjs
@@ -0,0 +1,15 @@
+import { AVG_CALLS_PER_HOUR, RANDOM_CALL_TIMES } from "./constants.mjs";
+
+// pseudo gaussian random function. 
+// this averages out the amount of calls per hour. Making a smooth curve.
+// the higher the amount of random calls.
+// The smoother the curve and the seamless the randomness is 
+
+export function gaussianRand() {
+  var remainingRandomCalls=RANDOM_CALL_TIMES;
+  var accumulator = 0;
+  while(remainingRandomCalls--){
+      accumulator+=Math.random();
+  }
+  return AVG_CALLS_PER_HOUR * accumulator / RANDOM_CALL_TIMES; 
+}
